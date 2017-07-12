a24b198a8f1777cd3a1ffff8ee89867d5407ac06
test <q38011039@mail.ncku.edu.tw>
2017-07-12 17:18:20 +0800

    Randomize Question Numbers Phase 5

--- Items.aspx.cs
+++ Items.aspx.cs
@@ -18,14 +18,34 @@ public partial class Items : Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
     protected void btnKnee_Click(object sender, EventArgs e)
     {
+
+
+        //Randomize the organ number picked by instructor 
+        int[] pickedQuestions = { 1, 3, 5 }; //The Question Number of organs  picked by instructor will be retrieved from DB in the near future. 
+        string ID_Num = "234";//The last 3 digits of student's ID will be retrieved from DB in the near future.  
+
+        int[] randomQuestionNo = RandomQuestionNo.rand(ID_Num, pickedQuestions);
+
+
+        //send randomized  Question Numbers picked by instructor to IPC.aspx through Session
+        Session["randomQuestionNo"] = randomQuestionNo;
+      
+        
+
+
         StreamWriter wr = (StreamWriter)Session["Writer"];
         wr.WriteLine("3 " + XMLFolder + questionXMLPath);//send protocol,Data to 3DBuilder.
-        //wr.WriteLine("3  " + questionXMLPath);//send protocol,Data to 3DBuilder.
+       
  //!!!!//如何將上方兩個參數傳送到3DBuilder那裏的IPCInterface呢? 上行的WriteLine會寫到哪裡呢?
-        Response.Redirect("IPC.aspx");
+
+        //head to IPC.aspx
+        //Response.Redirect("IPC.aspx");
+
+        ////head to IPC.aspx in exam mode
+        Response.Redirect("IPC.aspx?examMode=Yes");
     }
 }
