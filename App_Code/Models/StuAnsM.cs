f93e1801a524dbf7387cafaea6dfd579b34667d4
chiu <tim556612378@gmail.com>
2017-09-20 17:46:05 +0800

    upload new model StuAnsM to store student's answer to DB

---
+++ App_Code/Models/StuAnsM.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// StuAnsM 的摘要描述
+/// </summary>
+public class StuAnsM
+{
+   
+    public string _StudentAnswer;//儲存學生答案
+    public string _QuesOrdering;//儲存題號排序
+    //public string _StuCouHWDe_ID;
+    /*
+    public string StuCouHWDe_ID//儲存學生ID
+    {
+        get
+        {
+            return _StuCouHWDe_ID;
+        }
+        set
+        {
+            _StuCouHWDe_ID = value;
+        }
+    }
+    */
+    //public string StudentAnswer//儲存學生答案
+    //{
+    //    get
+    //    {
+    //        return _StudentAnswer;
+    //    }
+    //    set
+    //    {
+    //        _StudentAnswer = value;
+    //    }
+    //}
+    //public string QuesOrdering//儲存題號排序
+    //{
+    //    get
+    //    {
+    //        return _QuesOrdering;
+    //    }
+    //    set
+    //    {
+    //        _QuesOrdering = value;
+    //    }
+    //}
+    // Default constructor:
+    public StuAnsM()
+    {
+        _StudentAnswer = "";
+        _QuesOrdering = "";
+    }
+
+    //
+    // TODO: 在這裡新增建構函式邏輯
+    //
+}
