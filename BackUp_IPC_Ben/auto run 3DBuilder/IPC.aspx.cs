c3527b9ada500332caed680115e634c237f84dec
test <q38011039@mail.ncku.edu.tw>
2019-01-19 22:04:24 +0800

    update the comments in the auto run 3DBuilder version of the IPC.aspx.cs

--- BackUp_IPC_Ben/auto run 3DBuilder/IPC.aspx.cs
+++ BackUp_IPC_Ben/auto run 3DBuilder/IPC.aspx.cs
@@ -19,13 +19,13 @@ using System.Data.Entity;
 
 
 
-public partial class IPC : CsSessionManager 
+public partial class IPC : CsSessionManager
 {
     //private static System.Diagnostics.Process os = new System.Diagnostics.Process();
-   // public static System.Diagnostics.Process rd = new System.Diagnostics.Process();
-   // private StreamWriter myStreamWriter = null; 
+    // public static System.Diagnostics.Process rd = new System.Diagnostics.Process();
+    // private StreamWriter myStreamWriter = null; 
 
-    
+
     //In the near future ,we will get the Path from URL para or other para transmission method.
     string XMLFolder = CsDynamicConstants.AITypeQuestionXMLFolder;
     string ThreeDBuilderXMLFolder = CsDynamicConstants.ThreeDBuilderXMLFolder; //"D:\\IPC_interact_with_3DBuilder\\IPC_Questions\\1161-1450\\";//it's only used when we want the 3DBuilder to load the Organ XML
@@ -37,12 +37,12 @@ public partial class IPC : CsSessionManager
     string cActivityID = "009";
     */
     string cUserID = "";
-    string cQID ="";
+    string cQID = "";
 
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        questionXMLPath = Request["strQID"]+".xml";
+        questionXMLPath = Request["strQID"] + ".xml";
         QuestionFileName = questionXMLPath;
 
         questionXMLPath = XMLFolder + questionXMLPath;
@@ -59,7 +59,7 @@ public partial class IPC : CsSessionManager
             DataSet ds = new DataSet();
             //ds.ReadXml(Server.MapPath("SceneFile13.xml")); //must synchronized with the XML file in Items.aspx.cs:  wr.WriteLine("3 D:\\Mirac3DBuilder\\HintsAccounts\\Student\\Mirac\\1161-1450\\SceneFile13.xml");//send protocol,Data to 3DBuilder.
             ds.ReadXml(Server.MapPath(questionXMLPath)); //must synchronized with the XML file in Items.aspx.cs:  wr.WriteLine("3 D:\\Mirac3DBuilder\\HintsAccounts\\Student\\Mirac\\1161-1450\\SceneFile13.xml");//send protocol,Data to 3DBuilder.
-            
+
             //in Items.aspx.cs
 
             //DataRow dtRow = dtScore.NewRow();
@@ -72,7 +72,7 @@ public partial class IPC : CsSessionManager
 
             //to contain the content read from organ XML file.
             List<string> CorrectOrganNameList = new List<string>();
-   
+
             // For each correct organ name in the table, 
             //add it to a list called CorrectOrganNameList .
 
@@ -88,20 +88,19 @@ public partial class IPC : CsSessionManager
 
 
             //run the CSNamedPipe.exe to communicate with 3DBuilder.exe
-            activate3DBuilder(); 
-           
+            activate3DBuilder();
+
             /*Ben test
             //randomize the Question Organ Number
             randomizeQuestionOrganNo();
             */
 
 
-            //add an invisible row which contains a btn to activate ShowAll 
-            //use JS alert() in C#
-           
-           
+
+
+
         }
-        
+
 
 
         /*This block should be implemented in the Hints set timer for the exam page
@@ -109,7 +108,7 @@ public partial class IPC : CsSessionManager
         //get the exam timespan from the UI , and convert it to Second format.
         int hardCodeExamTimespanSec = 120000; //now I just hard code it to 15 sec
         Double examTimespan = hardCodeExamTimespanSec * 1.0;
-        DateTime deadlineDateTime=DateTime.Now.AddSeconds(examTimespan); // return Datetime format
+        DateTime deadlineDateTime = DateTime.Now.AddSeconds(examTimespan); // return Datetime format
         /*psedo code:
          * store deadlineDateTime to DB
          * */
@@ -124,9 +123,9 @@ public partial class IPC : CsSessionManager
         //check count down timer to decide whether time is already up.
         CheckCountDownTimer(deadlineDateTime);
 
-       
-        
-       
+
+
+
     }
 
     private void activate3DBuilder()
@@ -135,11 +134,11 @@ public partial class IPC : CsSessionManager
         runCSNamedPipe();
 
 
-       
+
         //run 3DBuilder.exe
         run3DBuilder();
 
-        /*
+        /*Ben test
        //originating from ALHomePage.aspx
        StreamWriter wr = (StreamWriter)Session["Writer"];
        //Page.ClientScript.RegisterStartupScript(this.GetType(), "", "<script type='text/javascript'>alert('" + os.StartInfo.RedirectStandardInput + os.Id + "');</script>"); 
@@ -147,7 +146,7 @@ public partial class IPC : CsSessionManager
 
        wr.WriteLine("1 2");
         */
-      
+
 
     }
 
@@ -169,27 +168,17 @@ public partial class IPC : CsSessionManager
         Session["Process"] = os;
 
 
-       
-        
+
+
 
     }
 
     private void run3DBuilder()
     {
-        /*
-        System.Diagnostics.Process process = new System.Diagnostics.Process();
-        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-        startInfo.FileName = "cmd.exe";
-        startInfo.Arguments = " \"D:\\3DBuilder\\[20120726]3DBuilder\\x64\\Debug\\3DBuilder.exe\" ";
-        startInfo.Verb = "runas";
-        process.StartInfo = startInfo;
-        process.Start();
 
-        */
 
         //this works but 3DBuilder will be run in background
-        
+
         System.Diagnostics.Process process = new System.Diagnostics.Process();
         System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
         startInfo.Verb = "runas";
@@ -201,17 +190,17 @@ public partial class IPC : CsSessionManager
         startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
         startInfo.CreateNoWindow = false;
         startInfo.UseShellExecute = true;
-        
-        startInfo.FileName = Request.MapPath("cmd.lnk");
+
+        startInfo.FileName = Request.MapPath("3DBuilder.lnk");
         //startInfo.FileName = "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\installutil.exe";
         //startInfo.Arguments = "D:\\Projects\\MyNewService\\bin\\Release\\MyNewService.exe";
         process.StartInfo = startInfo;
         bool processStarted = process.Start();
 
-      
-         
 
-      
+
+
+
     }
 
     //originating from Item.aspx btnKnee_Click()
@@ -305,18 +294,18 @@ public partial class IPC : CsSessionManager
 
     }
 
-    private  void  intArray2AString(int[] randomQuestionNo,ref string strRandomQuestionNo)
+    private void intArray2AString(int[] randomQuestionNo, ref string strRandomQuestionNo)
     {
         string[] result = randomQuestionNo.Select(x => x.ToString()).ToArray();
-       
-        for (int i=0; i < result.Length; i++)
+
+        for (int i = 0; i < result.Length; i++)
         {
             strRandomQuestionNo += result[i];
             strRandomQuestionNo += " ";
 
         }
 
-       
+
 
 
     }
@@ -327,7 +316,7 @@ public partial class IPC : CsSessionManager
     {
         ////Get The Question Number of organs  picked by instructor////////// 
         //read in the XML files that contains all organs of a certain body part. e.g., Knee 
-        XMLHandler xmlHandler = new XMLHandler(Server.MapPath(questionXMLPath ));
+        XMLHandler xmlHandler = new XMLHandler(Server.MapPath(questionXMLPath));
 
         //convert  XmlDocument object to XElement object to use "where" phrase to locate a specific tag;
         xmlHandler.convertXmlDoc2XElement();
@@ -339,7 +328,7 @@ public partial class IPC : CsSessionManager
 
     private void CheckCountDownTimer(DateTime deadlineDateTime)
     {
-        
+
         int serverSideRemainingTimeSec = Convert.ToInt32((deadlineDateTime - DateTime.Now).TotalSeconds);
 
         //if time is already up, force submit the AI type exam paper. 
@@ -352,12 +341,12 @@ public partial class IPC : CsSessionManager
         {
             //pass the server Side Remaining Time (Sec) to front end through hidden field
             hidden_serverSideRemainingTimeSec.Value = serverSideRemainingTimeSec.ToString();
-           
+
 
         }
 
     }
-   
+
 
 
     public void sendMsg23DBuilder(string contact)
@@ -385,29 +374,29 @@ public partial class IPC : CsSessionManager
         //send cmd1
         try
         {
-           
+
             StreamWriter wr = (StreamWriter)Session["Writer"];
             //StreamWriter wr = new StreamWriter((StreamWriter)Session["Writer"]);
             //StreamWriter wr = new StreamWriter((Stream )Session["Writer"], Encoding.UTF8, 4096, true);
             //send cmd2
             wr.WriteLine(contact);//!!!!!send update msg to 3DBuilder
-            
+
             // the streamwriter WILL be closed and flushed here, even if an exception is thrown.
-           
+
             //wr.Flush();
         }
         catch (Exception e)
         {
 
         }
-         
+
     }
 
     protected void StartIPC_Click(object sender, EventArgs e)
     {
-        
 
-     }
+
+    }
     protected void StartRemoteApp_ServerClick(object sender, EventArgs e)
     {
 
@@ -578,17 +567,17 @@ public partial class IPC : CsSessionManager
 
     public String switchVisible_Invisible(GridViewRow selectedRow, string HFID, GridView gvScore)
     {
-        bool BooLInOrVisible=false;
-       
-        if (selectedRow!=null)//hide or show the selected organ.
+        bool BooLInOrVisible = false;
+
+        if (selectedRow != null)//hide or show the selected organ.
         {
-            var HFInOrVisible =selectedRow.FindControl(HFID) as HiddenField;
+            var HFInOrVisible = selectedRow.FindControl(HFID) as HiddenField;
             BooLInOrVisible = Convert.ToBoolean(HFInOrVisible.Value);
         }
-       
+
 
         String HideOrShow = "";
-        if (BooLInOrVisible )//if current state is visible
+        if (BooLInOrVisible)//if current state is visible
         {
             //means its visible before being clicked
             if (selectedRow != null)
@@ -596,7 +585,7 @@ public partial class IPC : CsSessionManager
                 (selectedRow.FindControl(HFID) as HiddenField).Value = "false";//switch to invisible
                 HideOrShow = "hide";
             }
-           
+
         }
         else//if current state is invisible
         {
@@ -605,16 +594,16 @@ public partial class IPC : CsSessionManager
                 (selectedRow.FindControl(HFID) as HiddenField).Value = "true";//switch to visible
                 HideOrShow = "show";
             }
-            
+
             else
             {
                 foreach (GridViewRow row in gvScore.Rows)
-                    {
-                        (row.FindControl(HFID) as HiddenField).Value = "true";//switch to visible
-                    }
+                {
+                    (row.FindControl(HFID) as HiddenField).Value = "true";//switch to visible
+                }
                 HideOrShow = "showAll";
-             }            
-           
+            }
+
         }
         return HideOrShow;
     }
@@ -622,13 +611,13 @@ public partial class IPC : CsSessionManager
 
     protected void ShowOrHideAll_Click(object sender, EventArgs e)
     {
-        
 
-        
+
+
         //switch visibility icon All rows .
         String HideOrShow = switchVisible_Invisible(null, "InOrVisible", gvScore);
         string contact = "7 " + HideOrShow; //send "6 Hide realOrganName" to 3DBuilder  
-        
+
 
         /*
          string a = "abcdd";
@@ -648,16 +637,16 @@ public partial class IPC : CsSessionManager
         //string contact = "6 " + HideOrShow ;
 
 
-        
+
         //string contact = "6 hide Left Popliteal Vein";
-        
+
         sendMsg23DBuilder(contact);
 
-       // Thread.Sleep(1000); //Delay 1秒
-        
-       
-       
-       
+        // Thread.Sleep(1000); //Delay 1秒
+
+
+
+
     }
     //submit current edition to 3DBuilder
     protected void gvScore_RowCommand(Object sender, GridViewCommandEventArgs e)
@@ -679,7 +668,7 @@ public partial class IPC : CsSessionManager
             // cell in the GridView control.
             GridViewRow selectedRow = gvScore.Rows[index];
             var tbx = selectedRow.FindControl("TextBox_Text") as TextBox;
-            var num =selectedRow.FindControl("TextBox_Number") as Label;
+            var num = selectedRow.FindControl("TextBox_Number") as Label;
             /////////////////////////////////////////////
             //text exam mode
             //store the Question number
@@ -687,33 +676,33 @@ public partial class IPC : CsSessionManager
 
             //if it's in the exam mode
             if (Request["examMode"] == "Yes")
-             {
+            {
 
                 QuestionNo = (Array.IndexOf(randQuestionNoList, Int32.Parse(num.Text)) + 1).ToString();
                 // QuestionNo = index.ToString();
-                
+
             }
-            
+
             //if it's not in the exam mode
             else
-           {
-               QuestionNo = num.Text;
-           }
-             /////////////////////////////
-         
-           
+            {
+                QuestionNo = num.Text;
+            }
+            /////////////////////////////
+
+
             //get the corresponding correct organ name 
-           // var answer = CorrectOrganNameSession[Convert.ToInt32(QuestionNo) - 1];
+            // var answer = CorrectOrganNameSession[Convert.ToInt32(QuestionNo) - 1];
 
             var answer = CorrectOrganNameSession[Convert.ToInt32(QuestionNo)];
-                
-            
-            string input = tbx.Text.Replace(" ","_");
+
+
+            string input = tbx.Text.Replace(" ", "_");
             //Bent 2017 test
             string contact = "5 " + input + " " + QuestionNo + " " + answer.ToString();
             //string contact = "5 " + input + " "  + answer.Value.ToString();
             sendMsg23DBuilder(contact);
-               
+
         }
 
 
@@ -732,31 +721,31 @@ public partial class IPC : CsSessionManager
             // Get the last name of the selected author from the appropriate
             // cell in the GridView control.
             GridViewRow selectedRow = gvScore.Rows[index];
-           
+
             var num = selectedRow.FindControl("TextBox_Number") as Label; //Index of the selected 3D object
-            
+
             //get the corresponding correct organ name 
             var answer = CorrectOrganNameSession[Convert.ToInt32(num.Text) - 1];//The correct name of selected 3D object 
-            
+
             //switch visibility icon of the selected row .
-            String HideOrShow = switchVisible_Invisible(selectedRow, "InOrVisible",null);
-           
-           /*
-            //use JS alert() in C#
-            ScriptManager.RegisterStartupScript(this,
-             typeof(Page),
-             "Alert",
-             "<script>alert('" + (selectedRow.Controls[3]).ImageUrl + "');</script>",
-             false);
+            String HideOrShow = switchVisible_Invisible(selectedRow, "InOrVisible", null);
+
+            /*
+             //use JS alert() in C#
+             ScriptManager.RegisterStartupScript(this,
+              typeof(Page),
+              "Alert",
+              "<script>alert('" + (selectedRow.Controls[3]).ImageUrl + "');</script>",
+              false);
              
-            //send hide 3D organ msg to 3DBuilder
-            */
-            
+             //send hide 3D organ msg to 3DBuilder
+             */
+
             string contact = "6 " + HideOrShow + " " + answer.ToString(); //send "6 Hide realOrganName" to 3DBuilder
-            
-           sendMsg23DBuilder(contact);
-           
-            
+
+            sendMsg23DBuilder(contact);
+
+
 
         }
         if (e.CommandName == "relay")
@@ -766,7 +755,7 @@ public partial class IPC : CsSessionManager
             string contact = "7 " + HideOrShow; //send "6 Hide realOrganName" to 3DBuilder  
 
 
-           
+
             sendMsg23DBuilder(contact);
 
         }
